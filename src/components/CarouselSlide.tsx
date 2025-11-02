b6ee70613077a0ed2da9baeba6d2f6929af14bd5
Toufiq Choudhari <iamtoufiq2@gmail.com>
2025-11-02 19:41:40 +0530

    removed the spinner

--- src/components/CarouselSlide.tsx
+++ src/components/CarouselSlide.tsx
@@ -24,11 +24,11 @@ const CarouselSlide = memo<CarouselSlideProps>(
                 onLoad={() => setImageLoaded(true)}
                 onError={() => setImageLoaded(true)} // Show content even if image fails
               />
-              {!imageLoaded && (
+              {/* {!imageLoaded && (
                 <div className="absolute inset-0 bg-gray-200 animate-pulse flex items-center justify-center">
                   <div className="w-12 h-12 border-4 border-gray-300 border-t-gray-600 rounded-full animate-spin"></div>
                 </div>
-              )}
+              )} */}
               <div className="absolute inset-0 bg-linear-to-t from-black/20 to-transparent" />
             </div>
           )}
